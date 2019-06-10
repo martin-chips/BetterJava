7f45b2d2d28b206214abb8bd0a8f7b6df3f0e8bd
Dimple <bianxiaofeng@sohu.com>
2019-06-10 15:34:02 +0800

    第39条：注解优于命名模式

---
+++ src/main/java/com/dimple/effectiveJava/chapter6/Item38.java
@@ -0,0 +1,12 @@
+package com.dimple.effectiveJava.chapter6;
+
+/**
+ * @className: Item38
+ * @description: 用接口模拟可扩展的枚举
+ *
+ * @auther: Dimple
+ * @date: 06/10/19
+ * @version: 1.0
+ */
+public class Item38 {
+}
---
+++ src/main/java/com/dimple/effectiveJava/chapter6/Item39.java
@@ -0,0 +1,13 @@
+package com.dimple.effectiveJava.chapter6;
+
+/**
+ * @className: Item39
+ * @description: 注解优于命名模式
+ * 一般使用命名模式（naming pattern） 表名有些程序元素需要通过某种工具或者框架进行特殊处理。比如Java4发行之前，Junit测试框架要求要用test作为测试方法名称的开头，这种方法可行但有几
+ * 个缺点。首先，文字的拼写错误会导致失败，且没有提示。其次，它无法确保它们只用于相应的程序元素上。
+ * @auther: Dimple
+ * @date: 06/10/19
+ * @version: 1.0
+ */
+public class Item39 {
+}
