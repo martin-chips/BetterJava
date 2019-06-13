ce0c23c96b667a638cf334c505e1339b908f832b
Dimple <bianxiaofeng@sohu.com>
2019-06-13 15:59:25 +0800

    第56条：为所有的导出API元素编写文档注释

---
+++ src/main/java/com/dimple/effectiveJava/chapter8/Item56.java
@@ -0,0 +1,13 @@
+package com.dimple.effectiveJava.chapter8;
+
+/**
+ * @className: Item56
+ * @description: 为所有的导出API元素编写文档注释
+ * 1. 为了正确的编写API文档，必须要在每个被导出的类、接口、构造器、方法和域声明之前添加一个文档注释。
+ * 2. 方法中的文档注释应该简洁的描述出它和客户端之间的约定。
+ * @auther: Dimple
+ * @date: 06/13/19
+ * @version: 1.0
+ */
+public class Item56 {
+}
