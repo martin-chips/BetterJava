f5ef905cbd58161f23e6658b928f65093a792252
Dimple <bianxiaofeng@sohu.com>
2019-06-14 15:39:10 +0800

    第64条：通过接口引用对象

---
+++ src/main/java/com/dimple/effectiveJava/chapter9/Item64.java
@@ -0,0 +1,14 @@
+package com.dimple.effectiveJava.chapter9;
+
+/**
+ * @className: Item64
+ * @description: 通过接口引用对象
+ * 如果有合适的接口类型存在，那么对于参数、返回值、变量和域来说，就都应该使用接口类型进行声明。
+ * 如果养成用接口作为类型的习惯，程序将更加灵活，当需要更换实现的时候，要做的就只是更改构造器中类的名称。
+ * 如果没有合适的接口，就用类层次结构中提供必要功能的最小实现类来引用对象。
+ * @auther: Dimple
+ * @date: 06/14/19
+ * @version: 1.0
+ */
+public class Item64 {
+}
