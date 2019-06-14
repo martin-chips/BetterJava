647881b165074f8c358722ee72c4e1e9b078f263
Dimple <bianxiaofeng@sohu.com>
2019-06-14 16:16:48 +0800

    第68条：接受普遍的命名规则

---
+++ src/main/java/com/dimple/effectiveJava/chapter9/Item68.java
@@ -0,0 +1,12 @@
+package com.dimple.effectiveJava.chapter9;
+
+/**
+ * @className: Item68
+ * @description: 接受普遍的命名规则
+ * 参数类型通常由单个字符组成，这个字母通常是以下五种类型之一：T表示任意的类型、E表示集合的元素、K和V表示映射的键和值，X表示异常
+ * @auther: Dimple
+ * @date: 06/14/19
+ * @version: 1.0
+ */
+public class Item68 {
+}
