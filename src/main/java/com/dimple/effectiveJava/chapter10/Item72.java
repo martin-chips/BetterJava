604cc0622b1804773590e0806d226f0307c87971
Dimple <bianxiaofeng@sohu.com>
2019-06-14 17:31:42 +0800

    第72条：优先使用标准的异常

---
+++ src/main/java/com/dimple/effectiveJava/chapter10/Item72.java
@@ -0,0 +1,16 @@
+package com.dimple.effectiveJava.chapter10;
+
+/**
+ * @className: Item72
+ * @description: 优先使用标准的异常
+ * IllegalArgumentException 非null的参数值不正确
+ * IllegalStateException     不合适方法调用的对象状态
+ * NullPointException         在禁止使用Null的情况下参数值为null
+ * IndexOutOfBoundException   下标参数越界
+ * UnsupportedOperationException  对象不支持的请求
+ * @auther: Dimple
+ * @date: 06/14/19
+ * @version: 1.0
+ */
+public class Item72 {
+}
