b04a9634a2df516056751f30d328b797c283b3df
Dimple <bianxiaofeng@sohu.com>
2019-06-14 15:58:00 +0800

    第67条：谨慎的进行优化

---
+++ src/main/java/com/dimple/effectiveJava/chapter9/Item67.java
@@ -0,0 +1,11 @@
+package com.dimple.effectiveJava.chapter9;
+
+/**
+ * @className: Item67
+ * @description: 谨慎的进行优化
+ * @auther: Dimple
+ * @date: 06/14/19
+ * @version: 1.0
+ */
+public class Item67 {
+}
