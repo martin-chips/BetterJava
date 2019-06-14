3b861c9f4b22615cd99992dce2620fcc01f92592
Dimple <bianxiaofeng@sohu.com>
2019-06-14 13:57:33 +0800

    第57条：将局部变量的作用域最小化

---
+++ src/main/java/com/dimple/effectiveJava/chapter9/Item57.java
@@ -0,0 +1,14 @@
+package com.dimple.effectiveJava.chapter9;
+
+/**
+ * @className: Item57
+ * @description: 将局部变量的作用域最小化
+ * 1. 要使局部变量的作用域最小化，最有力的方法就是在第一次使用它的时候进行声明。
+ * 2. 几乎每一个局部两边的声明都应该包含一个初始化表达式。try-catch除外
+ * @auther: Dimple
+ * @date: 06/14/19
+ * @version: 1.0
+ */
+public class Item57 {
+
+}
