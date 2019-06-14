ee672be565c22c6cced18edb283c2e14425ee455
Dimple <bianxiaofeng@sohu.com>
2019-06-14 15:22:36 +0800

    第62条：如果其他类型更适合，尽量避免使用字符串

---
+++ src/main/java/com/dimple/effectiveJava/chapter9/Item62.java
@@ -0,0 +1,17 @@
+package com.dimple.effectiveJava.chapter9;
+
+/**
+ * @className: Item62
+ * @description: 如果其他类型更适合，尽量避免使用字符串
+ * 字符串不适合代替其他的值类型
+ * 字符串不适合代替枚举类型
+ * 字符串不适合代替聚合类型
+ * 字符串不适合代替能力表
+ * 总而言之：如果可以使用更加适合的数据类型，就编写更加适当的数据类型，就应该避免字符串表示对象。
+ * @auther: Dimple
+ * @date: 06/14/19
+ * @version: 1.0
+ */
+public class Item62 {
+
+}
