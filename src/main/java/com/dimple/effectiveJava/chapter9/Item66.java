3b35a1809de0ab86dfa1a6e90a250e25efa5b7a2
Dimple <bianxiaofeng@sohu.com>
2019-06-14 15:53:45 +0800

    第66条：谨慎使用本地方法

---
+++ src/main/java/com/dimple/effectiveJava/chapter9/Item66.java
@@ -0,0 +1,12 @@
+package com.dimple.effectiveJava.chapter9;
+
+/**
+ * @className: Item66
+ * @description: 谨慎使用本地方法
+ * Java Native Interface(JNI)允许Java应用程序调用本地方法。所谓本地方法就是使用本地编程语言如C、C++来编写的方法。
+ * @auther: Dimple
+ * @date: 06/14/19
+ * @version: 1.0
+ */
+public class Item66 {
+}
