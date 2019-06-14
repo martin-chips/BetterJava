47b128b044584640664e035b99f8112c1a39c1a2
Dimple <bianxiaofeng@sohu.com>
2019-06-14 17:01:16 +0800

    第69条：只针对异常的情况才使用异常

---
+++ src/main/java/com/dimple/effectiveJava/chapter10/Item69.java
@@ -0,0 +1,12 @@
+package com.dimple.effectiveJava.chapter10;
+
+/**
+ * @className: Item69
+ * @description: 只针对异常的情况才使用异常
+ * 异常只应该用于异常的情况，不应该用于正常的控制流
+ * @auther: Dimple
+ * @date: 06/14/19
+ * @version: 1.0
+ */
+public class Item69 {
+}
